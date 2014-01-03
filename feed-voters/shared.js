2abfa9fd04058f492d4a67e9cf8c3895c8f5a3d5
Jim Thoburn <jim@jimthoburn.com>
2014-01-03 13:22:22 -0800

    Add prototype for the feed with extra voter content

---
+++ feed-voters/shared.js
@@ -0,0 +1,156 @@
+
+(function() {
+
+  if (!document.body.addEventListener) return;
+
+
+  /* =DelayedHover
+  ----------------------------------------------- */
+  var DelayedHover = function(element, delay) {
+    if (!element) return;
+
+    var timer;
+    var active = true;
+    if (!delay) delay = 300;
+
+    function activate() {
+      if (active) return;
+      element.className += " active";
+      active = true;
+    };
+
+    function deactivate() {
+      if (!active) return;
+      element.className = element.className.replace(/ *active/g, "");
+      active = false;
+    };
+
+    deactivate();
+
+    element.addEventListener("mouseover", function(e) {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(activate, delay);
+    }, false);
+
+    element.addEventListener("mouseout", function(e) {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(deactivate, 1);
+    }, false);
+
+    element.addEventListener("click", function(e) {
+      if (timer) clearTimeout(timer);
+    }, false);
+
+    element.className += " scripted";
+  };
+
+  var votes = document.getElementsByClassName("votes");
+  for (var index = 0; index < votes.length; index++) {
+    new DelayedHover(votes[index]);
+  }
+
+  /*
+  var sources = document.getElementsByClassName("source");
+  for (var index = 0; index < sources.length; index++) {
+    new DelayedHover(sources[index]);
+  }
+
+  var numbers = document.getElementsByClassName("numbers");
+  for (var index = 0; index < numbers.length; index++) {
+    new DelayedHover(numbers[index]);
+  }
+
+  var tags = document.querySelectorAll(".tags li");
+  for (var index = 0; index < tags.length; index++) {
+    new DelayedHover(tags[index]);
+  }
+  */
+
+  //new DelayedHover(document.getElementById("account"), 50);
+
+})();
+
+
+(function() {
+
+  if (!document.body.addEventListener || !document.body.querySelector) return;
+
+/*  =ToggleButton
+  ----------------------------------------------- */
+  var ToggleButton = function(element, labels) {
+
+    if (!element) return;
+
+    var button = element.getElementsByTagName("button");
+    if (button.length > 0) button = button[0];
+
+    var cursor = 0;
+
+    function toggle() {
+      cursor++;
+      if (cursor > labels.length - 1) cursor = 0;
+      button.innerHTML = labels[cursor];
+    };
+
+    element.addEventListener("submit", function(e) { e.preventDefault(); }, false);
+
+    var span;
+    if (labels.length > 2) {
+      span = document.createElement("span");
+      button.parentNode.appendChild(span);
+      for (var index = 1; index < labels.length; index++) {
+        var temp = button.cloneNode(true);
+        temp.innerHTML = labels[index]
+        span.appendChild(temp);
+        /*
+        temp.addEventListener("click", function(e) {
+          activate(e);
+          e.preventDefault();
+        }, false);
+        */
+      }
+    } else {
+      button.addEventListener("click", function(e) {
+        toggle();
+        e.preventDefault();
+      }, false);
+    }
+
+  }
+
+  var articles = document.getElementsByTagName("article");
+  for (var index = 0; index < articles.length; index++) {
+
+    var form = articles[index].querySelector(".body .todo_actions form");
+    if (form) {
+      new ToggleButton(form, ["Do It", "To Do", "Done"]);
+    }
+
+    /*
+    form = articles[index].querySelector(".source .details form");
+    if (form) {
+      new ToggleButton(form, "Following", "Follow");
+    }
+
+    forms = articles[index].querySelectorAll(".object form");
+    for (var j = 0; j < forms.length; j++) {
+      new ToggleButton(forms[j], "Following", "Follow");
+    }
+    */
+
+    forms = articles[index].querySelectorAll(".person form");
+    for (var j = 0; j < forms.length; j++) {
+      new ToggleButton(forms[j], ["Follow", "Following"]);
+    }
+
+    forms = articles[index].querySelectorAll(".details form");
+    for (var j = 0; j < forms.length; j++) {
+      new ToggleButton(forms[j], ["Follow", "Following"]);
+    }
+
+  }
+
+})();
+
+
+
